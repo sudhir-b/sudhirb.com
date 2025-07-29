4464926a9efae2e2eea7316ad8401577a3fcc795
Sudhir Balaji <sud.balaji@gmail.com>
2025-07-30 00:47:19 +0100

    Start making some progress

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,46 @@
+import { pathToFileURL } from "node:url";
+import { evaluate } from "@mdx-js/mdx";
 import { renderToStaticMarkup } from "react-dom/server";
-import { register } from "node:module";
-
-register("@mdx-js/node-loader", import.meta.url);
+import * as runtime from "react/jsx-runtime";
 
 export default function (eleventyConfig) {
+  // MDX support
   eleventyConfig.addExtension("mdx", {
-    key: "11ty.js",
-    compile: () => {
+    compile: async (str, inputPath) => {
+      const { default: mdxContent } = await evaluate(str, {
+        ...runtime,
+        baseUrl: pathToFileURL(inputPath),
+      });
+
       return async function (data) {
-        let content = await this.defaultRenderer(data);
-        return renderToStaticMarkup(content);
+        let res = await mdxContent(data);
+        return renderToStaticMarkup(res);
       };
     },
   });
+
+  // Collections for blog and TIL
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("blog/**/*.mdx").reverse();
+  });
+
+  eleventyConfig.addCollection("til", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("til/**/*.mdx").reverse();
+  });
+
+  // Copy CSS file to output
+  eleventyConfig.addPassthroughCopy("styles.css");
+
+  // Directory configuration
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      layouts: "_includes",
+      output: "_site"
+    },
+    templateFormats: ["mdx"],
+    markdownTemplateEngine: false,
+    htmlTemplateEngine: false
+  };
 }
